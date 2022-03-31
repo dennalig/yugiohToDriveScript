28d4bcb145014585201b0bc39baef6206f2c7a09
dennalig <dennaligrissom7@gmail.com>
2022-03-31 00:16:37 -0400

    intial commit

---
+++ yugiohToDriveScript.js
@@ -0,0 +1,8 @@
+//https://developers.google.com/drive/api/v3/reference?apix=true
+
+//<.json file> <image file> <Google Drive file path>
+
+//default to custom sets folder
+
+console.log(process.argv);
+
